8157dbcfe6348c2d85412b5f231f50ada5c5de69
Gowtham <gowthama_thangam@epam.com>
2022-09-04 08:21:10 +0530

    correc path in subnets_for_ingress.sh

--- terraform/dev/subnets_for_ingress.sh
+++ terraform/dev/subnets_for_ingress.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-value=$(<subnets.txt)
-sed -i "s/{subnet_ids}/$value/g" ../../kubernetes/deployment/ingress.yaml
+value=$(<./terraform/dev/subnets.txt)
+sed -i "s/{subnet_ids}/$value/g" ./kubernetes/deployment/ingress.yaml
